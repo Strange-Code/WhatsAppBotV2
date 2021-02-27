01721810e66dc1eaa36833ccab50e51adaaaf0e0
gjimenez <gjimenez@zytrust.com>
2021-02-27 12:39:30 -0500

    actualización de boot

--- whatsappBot.py
+++ whatsappBot.py
@@ -1,58 +1,65 @@
-#DESARROLLADO POR COMUNIDAD DE PROGRAMADORES STRANGE CODE
 from selenium import webdriver
 import time
 
-browser = webdriver.Edge(executable_path="./driver/edgedriver")
+browser = webdriver.Edge(executable_path='./driver/msedgedriver')
 
+def seleccionarChat(nombre : str):
+    buscando = True
 
-def seleccionarChat():
-    elements = browser.find_elements_by_tag_name("span")
-    for element in elements:
-        if element.text == 'strngrwrld':
-            element.click()
-            return True
-    return False
+    while buscando:
+        print("BUSCANDO CHAT")
+        elements = browser.find_elements_by_tag_name("span")
+        for element in elements:
+            if element.text == nombre:
+                print("ENCONTRAMOS AL IMPOSTOR")
+                element.click()
+                buscando = False
+                break
 
+def enviar():
+    element = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button/span')
+    element.click()
+    print("MENSAJE ENVIADO")
+
+def enviarMensaje(mensaje: str):
+    chatbox = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+    chatbox.send_keys(mensaje)
+    time.sleep(2)
+    enviar()
+
+def leerArchivo(ruta:str):
+    archivo = open(ruta, mode='r', encoding='utf-8')
+    chatbox = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+
+    for linea in archivo.readlines():
+        print ("MENSAJE : ", linea)
+        chatbox.send_keys(linea)
+
+    archivo.close()
 
 def validaQR():
     try:
-        element = browser.find_element_by_tag_name("canvas")
+        browser.find_element_by_tag_name("canvas")
     except:
         return False
     return True
 
-def leerArchivo():
-    archivo = open('./resource/leer.txt',mode="r")
-    
-    print("SELECCIONAR LA CAJA DE TEXTO")
-    cajitaDeTexto = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-
-    for linea in archivo.readlines():
-        print("LINEA LEIDA : " + linea)
-        cajitaDeTexto.send_keys(linea)
-    archivo.close()
-
-def bootWhatsapp():
+def botWhatsapp():
     browser.get("https://web.whatsapp.com/")
     time.sleep(5)
 
-    ##variable de espera
     espera = True
 
     while espera:
-        print("ESTOY ESPERADO")
+        print("ESTOY ESPERANDO")
         espera = validaQR()
         time.sleep(2)
         if espera == False:
-            print("QUE SE AUTENTICO")
+            print("SE AUTENTICO")
             break
     
+    seleccionarChat("RPONCE")
     time.sleep(2)
-    print("BUSCANDO CHAT")
-    btnChat = seleccionarChat()
-    time.sleep(2)
-    
-    leerArchivo()
- 
+    leerArchivo('./resource/pruebaBot.txt')
 
-bootWhatsapp()
+botWhatsapp()
